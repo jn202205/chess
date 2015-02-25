a98315b9ba815934aaf3974beecb2cd5eb2d6692
Jonathan Newman <jn202205@gmail.com>
2015-02-25 08:51:00 -0800

    can move pawn

--- pawn.rb
+++ pawn.rb
@@ -1,32 +1,40 @@
 require_relative 'piece'
 
+
 class Pawn < Piece
   def initialize(color, pos, board)
     super
   end
 
   def valid_moves(pos)
-    all_valid_moves = []
-
     vertical_direction = (@color == :blue) ? 1 : -1
 
-    if can_move_forward?(pos, vertical_direction)
-      all_valid_moves << [pos.first, pos.last + vertical_direction]
-    end
-
-    if can_attack?
-    end
-
+    forward_moves(pos, vertical_direction) + attacking_moves(pos, vertical_direction)
   end
 
-  def can_attack?(vertical_direction)
+  def attacking_moves(pos, vertical_direction)
+    row, col = pos
+    diagonal_lt = [row + vertical_direction, col - 1]
+    diagonal_rt = [row + vertical_direction, col + 1]
+    attacking_positions = [diagonal_lt, diagonal_rt]
 
+    attacking_positions.select do |attack_pos|
+      @board[attack_pos] && @board[attack_pos].color != @color
+    end
   end
 
-  def can_move_forward?(pos, vertical_direction)
-    x, y = pos
-    front = @board[x][y + vertical_direction]
-    front.nil? && front[1].between(0, 7)
+  def forward_moves(pos, vertical_direction)
+    row, col = pos
+    raise ArgumentError.new("Invalid move: Can't move off board") if row + vertical_direction
+
+    front = @board[[row + vertical_direction, col]]
+    forward_moves = []
+
+    if front.nil?
+      forward_moves << [row + vertical_direction, col]
+    end
+
+    forward_moves
   end
 
   def show
